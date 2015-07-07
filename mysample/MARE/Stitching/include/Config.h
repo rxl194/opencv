db5c41df2a904540b4997b6e30a990c081c84aa8
rxl194 <Rendong_Lin@yahoo.com>
2015-07-06 22:53:39 -0700

    Signed-off-by: rxl194 <Rendong_Lin@yahoo.com>

--- mysample/MARE/Stitching/include/Config.h
+++ mysample/MARE/Stitching/include/Config.h
@@ -1,3 +1,3 @@
 // Preprocessor directives to set variables from values in the CMakeLists.txt files
 //#define DATA_FOLDER ""
-#define IMAGE_FOLDER "D:/git/ocv2.4.10/mysample/MARE/Stitching/stitching_img"
+#define IMAGE_FOLDER "C:\rxl194\git\ocv2.4.10\mysample\MARE\Stitching\stitching_img"
