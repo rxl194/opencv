1b070054aa945636426b2c8d215c98de1475e9de
rxl194 <Rendong_Lin@yahoo.com>
2016-09-02 23:15:18 +0000

    .

--- mysample/OpenCVApplicationProgramming/include/Config.h
+++ mysample/OpenCVApplicationProgramming/include/Config.h
@@ -1,3 +1,3 @@
 // Preprocessor directives to set variables from values in the CMakeLists.txt files
-#define DATA_FOLDER "D:/git/ocv2.4.10/mysample/OpenCVApplicationProgramming/data"
-#define IMAGE_FOLDER "D:/git/ocv2.4.10/mysample/OpenCVApplicationProgramming/images"
+#define DATA_FOLDER "/home/ystd2016/git/ocv2.4.10/mysample/OpenCVApplicationProgramming/data"
+#define IMAGE_FOLDER "/home/ystd2016/git/ocv2.4.10/mysample/OpenCVApplicationProgramming/images"
